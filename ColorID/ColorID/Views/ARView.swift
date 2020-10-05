4653522ba6c05ebc2e0eee908c0e0e4c2fc18516
brendon cecilio <brendon.cecilio@gmail.com>
2020-10-04 22:09:54 -0400

    organized files

---
+++ ColorID/ColorID/Views/ARView.swift
@@ -0,0 +1,20 @@
+//
+//  ARView.swift
+//  ColorID
+//
+//  Created by Brendon Cecilio on 10/4/20.
+//
+
+import UIKit
+
+class ARView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
